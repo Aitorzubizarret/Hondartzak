a6a26c83176c2cabfae6a99d60c40d37e1015935
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-18 23:41:41 +0200

    Added "MapTableViewCell" to the project.

---
+++ Hondartzak/Cells/MapTableViewCell/MapTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MapTableViewCell.swift
+//  Hondartzak
+//
+//  Created by Aitor Zubizarreta Perez on 18/04/2021.
+//
+
+import UIKit
+
+class MapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
