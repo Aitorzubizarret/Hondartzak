47a95abb0e54ccc42a243e901f77a11e7ad7e1a7
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-19 23:57:04 +0200

    Updated "MapTableViewCell".

--- Hondartzak/Cells/MapTableViewCell/MapTableViewCell.swift
+++ Hondartzak/Cells/MapTableViewCell/MapTableViewCell.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
 
 class MapTableViewCell: UITableViewCell {
     
     // MARK: - UI Elements
     
-    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var mapImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     // MARK: - Properties
+    
+    var snapShotter: MKMapSnapshotter {
+        let mapSnapshotOptions = MKMapSnapshotter.Options()
+
+        // Set the region of the map that is rendered.
+        let location = CLLocationCoordinate2DMake(43.305709,-2.366432) // Apple HQ
+        let region = MKCoordinateRegion(center: location, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        mapSnapshotOptions.region = region
+
+        // Set the scale of the image. We'll just use the scale of the current device, which is 2x scale on Retina screens.
+        mapSnapshotOptions.scale = UIScreen.main.scale
+
+        // Set the size of the image output.
+        mapSnapshotOptions.size = CGSize(width: 500, height: 100)
+
+        // Show buildings and Points of Interest on the snapshot
+        mapSnapshotOptions.showsBuildings = true
+        mapSnapshotOptions.showsPointsOfInterest = true
+
+        return MKMapSnapshotter(options: mapSnapshotOptions)
+    }
     
     // MARK: - Methods
     
@@ -33,6 +57,22 @@ class MapTableViewCell: UITableViewCell {
     /// Setup the View.
     ///
     private func setuptView() {
-        self.tempLabel.text = "Map"
+        // View.
+        self.mainView.backgroundColor = UIColor.red
+        self.mainView.layer.cornerRadius = 10
+        self.mainView.layer.masksToBounds = true
+        
+        // ImageView.
+        self.mapImageView.contentMode = .scaleAspectFill
+        self.snapShotter.start { (snapshot, error) in
+            if error == nil {
+                self.mapImageView.image = snapshot?.image
+            } else {
+                print("Error snapshot")
+            }
+        }
+        
+        // Label.
+        self.titleLabel.text = "Map"
     }
 }
