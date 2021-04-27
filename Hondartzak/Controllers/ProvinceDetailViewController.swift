977857d3aecb8998d551bf8bf4e95d9027849b55
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-27 23:14:25 +0200

    Updated "ProvinceDetailViewController" to setup the table view and display the name of the beaches in the table.

--- Hondartzak/Controllers/ProvinceDetailViewController.swift
+++ Hondartzak/Controllers/ProvinceDetailViewController.swift
@@ -20,15 +20,54 @@ class ProvinceDetailViewController: UIViewController {
             guard let province = self.province else { return }
             
             self.title = province.name.eu
+            
+            for city in province.cities {
+                for beach in city.beaches {
+                    self.beachesList.append(beach)
+                }
+            }
+            
         }
     }
+    private var beachesList: [Beach] = []
     
     // MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setupTableView()
+    }
+    
+    ///
+    /// Setup the TableView.
+    ///
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableView Delegate
+
+extension ProvinceDetailViewController: UITableViewDelegate {}
+
+// MARK: - UITableView Data Source
+
+extension ProvinceDetailViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.beachesList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = self.beachesList[indexPath.row].name.eu
+        return cell
     }
     
 }
