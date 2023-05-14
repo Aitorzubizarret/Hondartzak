13ac212d4f700283bbb87689bce18a4638d34848
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-05-14 13:04:07 +0200

    Create a new project in SwiftUI.

---
+++ Hondartzak/HondartzakApp.swift
@@ -0,0 +1,17 @@
+//
+//  HondartzakApp.swift
+//  Hondartzak
+//
+//  Created by Aitor Zubizarreta on 2023-05-14.
+//
+
+import SwiftUI
+
+@main
+struct HondartzakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
